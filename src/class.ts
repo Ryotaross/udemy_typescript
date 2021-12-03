570a51b32396e3c50880f674a97b93595bc4f438
Ryotaro Shimizu <ryotaro_625@icloud.com>
2021-12-03 23:07:31 +0900

    section4完

--- src/class.ts
+++ src/class.ts
@@ -1,17 +1,49 @@
-class Person {
-
-  name: string;
-
-  constructor(initName: string) {
-    this.name = initName;
+abstract class Person {
+  static spacies = 'Homo sapiens';
+  static isAdult(age: number) {
+    if(age > 17) return true;
+    return false;
+  }
+  constructor(public readonly name: string,protected age: number) {
   }
 
-  greeting(){
-    console.log(`Hello! My name is ${this.name}`)
+  incrementAge(){
+    this.age += 1;
+  }
+  greeting(this: Person){
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`)
+    this.explainJob();
+  }
+  abstract explainJob(): void;
+}
+
+class Teacher extends Person {
+  private static Instance: Teacher;
+  explainJob(){
+    console.log(`I am a teacher and I teach ${this.subject}`);
+  }
+  get subject(): string{
+    if(!this._subject) {
+      throw new Error('There is no subject.');
+    }
+    return this._subject;
+  }
+
+  set subject(value){
+    if(!value) {
+      throw new Error('There is no subject.');
+    }
+    this._subject = value;
+  }
+  private constructor(name: string, age: number,private _subject: string){
+    super(name,age);
+  }
+  static getInstance(){
+    if(Teacher.Instance) return Teacher.Instance;
+    Teacher.Instance = new Teacher ('Ryo',25,'Infomation');
+    return Teacher.Instance;
   }
 }
 
-const quili = new Person('Quili');
-console.log(quili);
-
-quili.greeting();
+const teacher = Teacher.getInstance();
+teacher.greeting();
